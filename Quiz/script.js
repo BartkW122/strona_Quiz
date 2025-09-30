0314fa82bb2fcf749f4f7429b00c2a49b8bb7081
Bartek <bartoszwiadernylodz@gmail.com>
2025-09-30 22:02:11 +0200

    dodanie wysiwtlania pytac i odpowiedzi z checkboxami

--- Quiz/script.js
+++ Quiz/script.js
@@ -1,14 +1,36 @@
 $(document).ready(function(){
-    let div = document.createElement("div");
-    let h3 = document.createElement("h3");
-    let input = document.createElement("input");
-    let label = document.createElement("label");
-
     $.getJSON('pytania.json', function(data){
-        $.each(data,function(key, val) {
-            $.each(val,function(key2,val2){
-                
-            })
+        $.each(data, function(key, val) {
+            $.each(val, function(key2, val2){
+               
+                let div = document.createElement("div");
+                let h3 = document.createElement("h3");
+
+                h3.innerHTML = val2.question;
+                div.setAttribute("id", `pytanie-${key2}`);
+                div.appendChild(h3);
+
+
+                val2.options.forEach((element, index) => {
+                    let input = document.createElement("input");
+                    let label = document.createElement("label");
+                    let br = document.createElement("br"); 
+
+                    input.setAttribute("type", "checkbox");
+                    input.setAttribute("name", `odpowiedz-${key2}`);
+                    input.setAttribute("id", `id-${key2}-${index}`);
+                    input.value = element;
+
+                    label.setAttribute("for", `id-${key2}-${index}`);
+                    label.innerHTML = element;
+
+                    div.appendChild(input);
+                    div.appendChild(label);
+                    div.appendChild(br);
+                });
+
+                document.body.appendChild(div);
+            });
         });
     });
 });
