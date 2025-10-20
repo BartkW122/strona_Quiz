bfe03b50498b87eca2bad0ad5e462c90b2b3ad98
Bartek <bartoszwiadernylodz@gmail.com>
2025-10-20 17:00:34 +0200

    dodanie

--- Quiz/script.js
+++ Quiz/script.js
@@ -1,38 +1,137 @@
-$(document).ready(function(){
-    $.getJSON('pytania.json', function(data){
-        $.each(data, function(key, val) {
-            $.each(val, function(key2, val2){
-               
-                let div = document.createElement("div");
-                let h3 = document.createElement("h3");
+function losowanie_indeksow() {
+  let losowane_indexy = [];
 
-                h3.innerHTML = val2.question;
-                div.setAttribute("id", `pytanie-${key2}`);
-                div.appendChild(h3);
+  while (losowane_indexy.length < 20) {
+    let liczba = Math.floor(Math.random() * 20); 
+    if (!losowane_indexy.includes(liczba)) {
+      losowane_indexy.push(liczba);
+    }
+  }
 
+  return losowane_indexy;
+}
 
-                val2.options.forEach((element, index) => {
+function sprawdzenie_ilosci_odpowiedzi(dlugosc){
+    if(dlugosc == 1){
+        return true
+    }
+    if(dlugosc > 1){
+        return false
+    }
+}
+function  obsluga_button(){
+
+    let ilosc_klik = 1;
+    $("button.next").on("click",function(){
+        console.log(ilosc_klik);
+        if(ilosc_klik<=20){
+
+            ilosc_klik ++;
+
+        }else{
+
+            ilosc_klik = 1;
+
+            alert("koniec")
+            let btn_powtorz = document.createElement("button");
+
+            btn_powtorz.textContent = "Powtorz";
+            btn_powtorz.id = "powtorz";
+
+            $("div#conatiner").append(btn_powtorz);
+
+            $("button#powtorz").on("click",function(){
+                
+                    $("div#pytania").empty();
+                    $(this).remove();
+                    generowanie_pytan();
+
+            })
+
+        }
+        console.log(this)
+    })
+}
+
+function obsluga_input(tab){
+    $("input").on("click",function(){
+        console.log($(this))
+        if(!tab.includes($(this))){
+            tab.push($(this));
+        }
+    })
+}
+
+function generowanie_pytan(){
+    let tab_odpowiedzi = [];
+     let tab_indexy = losowanie_indeksow();
+     let liczba = 0;
+    $.getJSON('pytania.json', function(data) {
+            let pytania = data.quiz;
+
+            tab_indexy.forEach(index => {
+            let pytanie = pytania[index];
+            
+            let div = document.createElement("div");
+            let h3 = document.createElement("h3");
+            let button = document.createElement("button");
+            
+            button.textContent = "Dalej";
+            button.classList.add("next");
+            liczba++;
+            h3.textContent = `${liczba}, ${pytanie.question}`
+            
+            div.appendChild(h3);
+            
+            if(sprawdzenie_ilosci_odpowiedzi(pytanie.correct_answers.length)){
+                pytanie.options.forEach((opcja, i) => {
+
                     let input = document.createElement("input");
+                    input.type = "radio";
+                    input.name = `odpowiedz-${index}`;
+                    input.id = `id-${index}-${i}`;
+                    input.value = opcja;
+
                     let label = document.createElement("label");
-                    let br = document.createElement("br"); 
-
-                    input.setAttribute("type", "checkbox");
-                    input.setAttribute("name", `odpowiedz-${key2}`);
-                    input.setAttribute("id", `id-${key2}-${index}`);
-                    input.setAttribute("value", `${key2}`);
-                    input.value = element;
-
-                    label.setAttribute("for", `id-${key2}-${index}`);
-                    label.innerHTML = element;
+                    label.setAttribute("for", input.id);
+                    label.textContent = opcja;
 
                     div.appendChild(input);
                     div.appendChild(label);
-                    div.appendChild(br);
-                    console.log(div)
+                    div.appendChild(document.createElement("br"));
                 });
 
-                document.body.appendChild(div);
-            });
+            }else{
+                 pytanie.options.forEach((opcja, i) => {
+                    let input = document.createElement("input");
+                    input.type = "checkbox";
+                    input.name = `odpowiedz-${index}`;
+                    input.id = `id-${index}-${i}`;
+                    input.value = opcja;
+
+                    let label = document.createElement("label");
+                    label.setAttribute("for", input.id);
+                    label.textContent = opcja;
+
+                    div.appendChild(input);
+                    div.appendChild(label);
+                    div.appendChild(document.createElement("br"));
+                });
+
+            }
+
+            div.appendChild(button)
+    
+            $("div#pytania").append(div)
+
         });
+
+        obsluga_button();
+        obsluga_input(tab_odpowiedzi);
     });
+}
+$(document).ready(function() {
+    generowanie_pytan();
 });
+
+
